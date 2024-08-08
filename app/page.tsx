082dd89f386f892afeb00122acaac3956f069bb1
Zachary Sturman <zasturman@gmail.com>
2024-08-09 05:48:20 +1000

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+return (
+  <div className="flex items-center justify-center h-full">
+   <h1 className="text-3xl">
+     Zachary Sturman Portfolio.
+    </h1>
+  </div>
+)
+}
