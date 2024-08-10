f62b3f19ae30bf37f13c02971103368b3910a3bb
Zachary Sturman <zasturman@gmail.com>
2024-08-10 14:34:33 +1000

    Updates to workflow

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 return (
   <div className="flex items-center justify-center h-full">
    <h1 className="text-3xl">
-     Zachary Sturman Portfolio.
+     Zachary Sturman Portfolio!
     </h1>
   </div>
 )
