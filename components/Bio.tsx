dce9d9be013d9ba44cf22cfceb1a3c0984ee9ee4
Zachary Sturman <zasturman@gmail.com>
2025-02-23 16:03:10 -0800

    Fix to font-sizes

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -5,29 +5,29 @@ const Bio = () => {
 
     <div className="text-center sm:text-left text-3xl sm:text-2xl md:text-3xl sm:tracking-widest mb-2">
     <div className="flex flex-col items-center p-10  mt-10  max-w-[900px] mx-auto">
-      <h2 className="text-4xl font-bold mb-6"> I&apos;m Zachary Sturman, </h2>
+      <h2 className="text-lg md:text-xl lg:text-4xl font-bold mb-6"> I&apos;m Zachary Sturman, </h2>
 
 
 <div className="px-10">
 
 
-<p className="text-lg text-left leading-relaxed">
+<p className="text-sm md:text-base lg:text-lg text-left leading-relaxed">
             a full-stack developer and data engineer with expertise in building scalable applications, machine learning pipelines, and backend systems. 
           </p>
 
-          <p className="text-lg text-left leading-relaxed mt-4">
+          <p className="text-sm md:text-base lg:text-lg text-left leading-relaxed mt-4">
             My work includes building LLM-powered assistants, optimizing data architectures, and developing end-to-end ML solutions.
           </p>
         <div>
-            <p className="text-lg text-left leading-relaxed mt-4">Outside of programming, I enjoy:</p>
-            <ul className="text-lg text-left leading-relaxed list-disc pl-8">
+            <p className="text-sm md:text-base lg:text-lg text-left leading-relaxed mt-4">Outside of programming, I enjoy:</p>
+            <ul className="text-sm md:text-base lg:text-lg text-left leading-relaxed list-disc pl-8">
               <li>Writing and animating</li>
               <li>Exploring neuroscience & quantum physics</li>
               <li>Helping animals and supporting non-profit initiatives</li>
             </ul>
           </div>
 
-        <p className="text-lg text-left leading-relaxed mt-4">
+        <p className="text-sm md:text-base lg:text-lg text-left leading-relaxed mt-4">
             I am committed to solving real-world problems through technology, education, and creative solutions.
           </p>
 
